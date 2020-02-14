1c826d1681856eadf63f070e8682260a44a101cb
Lenny Lee <lennylee91@gmail.com>
2020-02-14 07:29:02 +0000

    migrate and seed db

---
+++ todolist/db/seeds.rb
@@ -0,0 +1,14 @@
+#I will create seed data to work with and test asoociations
+
+
+lenny = User.create(name: "Lenny", email: "lennylee91@gmail.com", password: "213")
+nelly = User.create(name: "Nelly", email: "AnellyPerez123@gmail.com", password: "213")
+
+Task.create(content: "Today is Thursday", user_id: lenny.id)
+
+lenny.tasks.create(content: "Building a Sinatra app")
+
+
+nellys_entry = nelly.tasks.build(content: "Ahh bubushka!")
+nellys_entry.save
+
