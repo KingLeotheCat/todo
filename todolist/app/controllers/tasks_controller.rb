7c6d5ad588a2f8430d1064a49f8f13901d827cc7
Lenny Lee <lennylee91@gmail.com>
2020-02-26 09:08:38 +0000

    Develop tasks controller

--- todolist/app/controllers/tasks_controller.rb
+++ todolist/app/controllers/tasks_controller.rb
@@ -5,7 +5,23 @@ class TasksController < ApplicationController
     erb :'/tasks/new'
   end
   
+  #Create a new entry
   post '/tasks' do
+    
+    #I want to create a new task and save it to DB
+    #I only want to save the entry if it has some content
+    if !logged_in?
+      redirect '/'
+    end
+    
+    if params[:content] !=""
+      #Create a new entry
+      @task = Task.create(content: params[:content], user_id: current_user.id)
+      redirect "/tasks/#{@task.id}"
+    else
+      redirect '/tasks/new'
+    end
+    #I also only want to create a task if a user is logged in
     
   end
   
