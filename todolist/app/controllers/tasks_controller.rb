9033522d7ef5b625ec63226a3d682840563d9dfa
Lenny Lee <lennylee91@gmail.com>
2020-02-28 06:44:55 +0000

    fixed task routes

--- todolist/app/controllers/tasks_controller.rb
+++ todolist/app/controllers/tasks_controller.rb
@@ -1,9 +1,13 @@
 class TasksController < ApplicationController
   
+  
+  
   #get tasks/new to render a form to create new task
   get '/tasks/new' do
     erb :'/tasks/new'
   end
+  
+  
   
   #Create a new entry
   post '/tasks' do
@@ -22,33 +26,53 @@ class TasksController < ApplicationController
       redirect '/tasks/new'
     end
     #I also only want to create a task if a user is logged in
-    
   end
+  
   
 
 
   #show route for a task 
   get '/tasks/:id' do
     @task = Task.find(params[:id])
-     erb :'/tasks/show'
+    erb :'/tasks/show'
   end
+  
+  
+  
   
   get '/tasks/:id/edit' do
     @task = Task.find(params[:id])
-    erb :'tasks/edit'
-  end
+    if logged_in?
+      if @task.user == current_user
+        erb :'tasks/edit'
+      else
+        redirect "users/#{current_user.id}"
+       end   
+    else
+      redirect '/'
+    end 
+  end  
+     
 
 
   patch '/tasks/:id' do
     #find task
     @task = Task.find(params[:id])
-    #modify the task
-    @task.update(content: params[:content])
-    #redirect to show page
-    redirect "/tasks/#{@task.id}"
+    if logged_in?
+      if @task.user==current_user
+        #modify the task
+        @task.update(content: params[:content])
+        #redirect to show page
+       redirect "/tasks/#{@task.id}"
+      else
     
+      redirect "/tasks/#{current_user.id}"
+    
+      end
+    else
+      redirect '/'
+    end
   end
-  
   
   #---------------------------
   
